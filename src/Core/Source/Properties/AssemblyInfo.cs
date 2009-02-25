f9632609161c91dd12b4a32774b538504bdcc316
dcazzulino <devnull@localhost>
2009-02-25 20:46:00 +0000

    CI: updated AssemblyInfo version to 0.1.225.1

--- src/Core/Source/Properties/AssemblyInfo.cs
+++ src/Core/Source/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyProduct("Funq")]
 [assembly: AssemblyTrademark("")]
 
-[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyVersion("0.1.225.1")]
 [assembly: AssemblyFileVersion("0.1.0.0")]
 
 [assembly: ComVisible(false)]
