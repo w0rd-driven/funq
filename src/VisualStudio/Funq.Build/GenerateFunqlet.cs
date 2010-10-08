80df235b5f8e079a86d6e327e38d2c469304fc26
dcazzulino <devnull@localhost>
2010-10-08 19:38:46 -0300

    Updated the license information in composition files, renamed task on build, etc.

--- src/VisualStudio/Funq.Build/GenerateFunqlet.cs
+++ src/VisualStudio/Funq.Build/GenerateFunqlet.cs
@@ -1,25 +1,81 @@
-﻿using System;
-using System.Linq;
-using Microsoft.Build.Utilities;
-using Microsoft.Build.Framework;
-using System.Reflection;
-using System.IO;
+﻿#region License
+// Microsoft Public License (Ms-PL)
+// 
+// This license governs use of the accompanying software. If you use the software, you accept this license. If you do not accept the license, do not use the software.
+// 
+// 1. Definitions
+// 
+//   The terms "reproduce," "reproduction," "derivative works," and "distribution" have the same meaning here as under U.S. copyright law.
+// 
+//   A "contribution" is the original software, or any additions or changes to the software.
+// 
+//   A "contributor" is any person that distributes its contribution under this license.
+// 
+//   "Licensed patents" are a contributor's patent claims that read directly on its contribution.
+// 
+// 2. Grant of Rights
+// 
+//   (A) Copyright Grant- Subject to the terms of this license, including the license conditions and limitations in section 3, each contributor grants you a non-exclusive, worldwide, royalty-free copyright license to reproduce its contribution, prepare derivative works of its contribution, and distribute its contribution or any derivative works that you create.
+// 
+//   (B) Patent Grant- Subject to the terms of this license, including the license conditions and limitations in section 3, each contributor grants you a non-exclusive, worldwide, royalty-free license under its licensed patents to make, have made, use, sell, offer for sale, import, and/or otherwise dispose of its contribution in the software or derivative works of the contribution in the software.
+// 
+// 3. Conditions and Limitations
+// 
+//   (A) No Trademark License- This license does not grant you rights to use any contributors' name, logo, or trademarks.
+// 
+//   (B) If you bring a patent claim against any contributor over patents that you claim are infringed by the software, your patent license from such contributor to the software ends automatically.
+// 
+//   (C) If you distribute any portion of the software, you must retain all copyright, patent, trademark, and attribution notices that are present in the software.
+// 
+//   (D) If you distribute any portion of the software in source code form, you may do so only under this license by including a complete copy of this license with your distribution. If you distribute any portion of the software in compiled or object code form, you may only do so under a license that complies with this license.
+// 
+//   (E ) The software is licensed "as-is." You bear the risk of using it. The contributors give no express warranties, guarantees or conditions. You may have additional consumer rights under your local laws which this license cannot change. To the extent permitted under your local laws, the contributors exclude the implied warranties of merchantability, fitness for a particular purpose and non-infringement.
+#endregion
+
+using System;
 using System.Collections.Generic;
-using System.Text;
 using System.ComponentModel.Composition;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
 using Funq.Build.Properties;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
 
 namespace Funq.Build
 {
-	public class FunqletGenerator : AppDomainIsolatedTask
+	/// <summary>
+	/// Generates the source code for a funqlet that registers all exported types 
+	/// in the calling project copy local references.
+	/// </summary>
+	public class GenerateFunqlet : AppDomainIsolatedTask
 	{
+		/// <summary>
+		/// Gets or sets the output path to leave the generated source code.
+		/// </summary>
 		public string OutputPath { get; set; }
+
+		/// <summary>
+		/// Gets or sets the namespace of the generated funqlet.
+		/// </summary>
 		public string TargetNamespace { get; set; }
+
+		/// <summary>
+		/// Gets or sets the assembly references to process.
+		/// </summary>
+		/// <value>The references.</value>
 		public ITaskItem[] References { get; set; }
 
+		/// <summary>
+		/// Gets the generated funqlet source file.
+		/// </summary>
 		[Output]
 		public ITaskItem Funqlet { get; private set; }
 
+		/// <summary>
+		/// Runs the task.
+		/// </summary>
 		public override bool Execute()
 		{
 			var exportedTypes = this.References
